f58cff4f707652640dc235877130e5cb975a1502
Lizadel Yarisantos <yarisantos@gmail.com>
2021-06-12 17:47:25 -0600

    Boostrap example

---
+++ react-bootstrap-tutorial-stickheader/src/App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, Row, Col} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container>
+              <Row>
+                <Col>1 of 1</Col>
+            </Row>
+        </Container>
+      </header>
+    </div>
+  );
+}
+
+export default App;
